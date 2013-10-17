d52abd2c741afbd4255e715b95e4021361cbc2d0
aude <aude.wiki@gmail.com>
2013-10-17 22:51:36 +0200

    cleanup config

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,9 @@
 // save as config.php
 
 $db_host = 'localhost';
-$db_user = 'uwikimania';
-$db_pass = '09ft9kkj';
-$db_name = 'dbwikimania';
+$db_user = 'wikimania';
+$db_pass = 'password';
+$db_name = 'scholarships';
 $db_driver = 'mysql';
 
 $open_time = gmmktime(0, 0, 0, /*january*/ 1, /*1st*/ 1, 2012);
@@ -12,13 +12,11 @@ $close_time = gmmktime(0, 0, 0, /*February*/ 2, /*23rd*/ 23, 2013);
 
 $email_from = 'wikimania-scholarships@wikimedia.org';
 
-$chapters_application = FALSE;
-
+$chapters_application = false;
 
 $TEMPLATEDIR = BASEDIR . '/templates/';
 $TEMPLATEBASE = 'templates/';
 $BASEURL = "/";
 $TEMPLATEBASE_REVIEW = $BASEURL . 'templates/';
 
-$mock = FALSE;
-?>
+$mock = false;
