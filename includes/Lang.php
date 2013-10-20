629abfeafe98c05d6e0a4ee37bd9755542082eee
aude <aude.wiki@gmail.com>
2013-10-20 15:03:15 -0700

    code style cleanup

--- includes/Lang.php
+++ includes/Lang.php
@@ -2,8 +2,9 @@
 
 class Lang {
 
-	var $messages = array();
-	var $lang;
+	protected $messages = array();
+
+	protected $lang;
 
 	function __construct() {
 		$this->loadMessages();
@@ -11,51 +12,55 @@ class Lang {
 	}
 
 	function langDir() {
-        	$langdir = dirname(__FILE__) . '/../languages';
-	        return $langdir;
+		$langdir = __DIR__ . '/../languages';
+		return $langdir;
 	}
 
 	function langFiles() {
-        	$langfiles = scandir($this->langDir());
-	        return $langfiles;
+		$langfiles = scandir( $this->langDir() );
+		return $langfiles;
 	}
 
 	function getLangs() {
-        	$langs = array();
-	        foreach ( $this->langFiles() as $file ) {
-        	        if (is_file($this->langDir() . '/' . $file)) {
-                	        $parts = explode(".", $file);
-                        	array_push($langs, $parts[0]);
-	                }
-	        }
+		$langs = array();
 
-        	return $langs;
+		foreach ( $this->langFiles() as $file ) {
+			if ( is_file( $this->langDir() . '/' . $file ) ) {
+				$parts = explode( '.', $file );
+				array_push( $langs, $parts[0] );
+			}
+		}
+
+		return $langs;
 	}
 
 	function loadMessages() {
-	        foreach ( $this->langFiles() as $file ) {
+		foreach ( $this->langFiles() as $file ) {
 			$fullname = $this->langDir() . '/' . $file;
-			if (is_file($fullname)) {
-		               	include( $fullname );
+
+			if ( is_file( $fullname ) ) {
+				include ( $fullname );
 			}
-	        }
+		}
+
 		$this->messages = $messages;
 	}
 
 	public function setLang($req) {
-        	if ( isset( $req['uselang'] )  && in_array( $req['uselang'], $this->getLangs() ) ) {
-                	$lang = $req['uselang'];
-	        } else {
-        	        $lang = 'en';
-	        }
+		if ( isset( $req['uselang'] ) && in_array( $req['uselang'], $this->getLangs() ) ) {
+			$lang = $req['uselang'];
+		} else {
+			$lang = 'en';
+		}
+
 		$this->lang = $lang;
+
 		return $lang;
 	}
 
-
 	public function message( $key ) {
-		if (isset( $this->messages[$this->lang][$key] ) ) {
-	        	return $this->messages[$this->lang][$key];
+		if ( isset( $this->messages[$this->lang][$key] ) ) {
+			return $this->messages[$this->lang][$key];
 		} else {
 			return $this->messages['en'][$key];
 		}
@@ -66,4 +71,3 @@ class Lang {
 	}
 
 }
-?>
