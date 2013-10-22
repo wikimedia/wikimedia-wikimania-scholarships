6def8aac0b38622f0bd4f4c0d4aadd776b21a762
Bryan Davis <bd808@wikimedia.org>
2013-10-21 19:05:21 -0600

    Fix form fields
    Address should be a textfield and presentationTopic isn't required.

--- templates/apply.php
+++ templates/apply.php
@@ -104,7 +104,7 @@ if ( $submitted != true ) {
 	$contact .= $templateHelper->getField( $app, 'name-last', 'lname', $values['lname'], true );
 	$contact .= $templateHelper->getField( $app, 'form-email', 'email', $values['email'], true );
 	$contact .= $templateHelper->getField( $app, 'form-telephone', 'telephone', $values['telephone'] );
-	$contact .= $templateHelper->getField( $app, 'form-mailing-address', 'address', $values['address'] );
+	$contact .= $templateHelper->getTextArea( $app, 'form-mailing-address', 'address', $values['address'] );
 
 	$contact .= $templateHelper->getResidenceHtml( $app, $COUNTRY_NAMES, $values['residence'], true );
 
@@ -174,7 +174,7 @@ if ( $submitted != true ) {
 
 	$interest .= $templateHelper->getYearsAttended( $attended );
 	$interest .= $templateHelper->getCheckbox( 'form-presenter', 'presentation', $values['presentation'] );
-	$interest .= $templateHelper->getField( $app, 'form-presentation-topic', 'presentationTopic', $values['presentationTopic'], true );
+	$interest .= $templateHelper->getField( $app, 'form-presentation-topic', 'presentationTopic', $values['presentationTopic'], false );
 	$interest .= $templateHelper->getHowHeard( $values['howheard'] );
 	$interest .= $templateHelper->getTextArea( $app, 'form-enrichment', 'why', $values['why'], true );
 
