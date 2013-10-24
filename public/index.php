1ab549a8ed11c573b32f776ed9f62e6a28cf0ab9
Bryan Davis <bd808@wikimedia.org>
2013-10-24 10:37:57 -0600

    Remove some unused bits in index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
 
-define( 'WMSCHOLS', true );
-
-require_once( "../src/init.php" );
+require_once '../src/init.php';
 
 $router = new Router( $BASEURL, $routes, $defaultRoute );
 $path = $router->route();
 $basepath = array_search( $path, $routes );
-$baselink = $BASEURL . $basepath;
 
 include $path;
