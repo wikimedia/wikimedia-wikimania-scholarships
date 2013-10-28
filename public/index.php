8dd38959695939d9ce3b0edc1ee8b71286fcf69c
Bryan Davis <bd808@wikimedia.org>
2013-10-28 16:03:21 -0600

    Add COPYING
    Added GPLv3 COPYING file and a full GPLv3 header to the main entry point
    script.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * @section LICENSE
+ * This file is part of Wikimania Scholarship Application.
+ *
+ * Wikimania Scholarship Application is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Wikimania Scholarship Application is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Wikimania Scholarship Application.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @file
+ * @copyright © 2013 Bryan Davis and Wikimedia Foundation.
+ * @copyright © 2013 Calvin W. F. Siu, Wikimania 2013 Hong Kong organizing team
+ * @copyright © 2012-2013 Katie Filbert, Wikimania 2012 Washington DC organizing team
+ * @copyright © 2011 Harel Cain, Wikimania 2011 Haifa organizing team
+ * @copyright © 2010 Wikimania 2010 Gdansk organizing team
+ * @copyright © 2009 Wikimania 2009 Buenos Aires organizing team
+ */
 
 require_once '../src/init.php';
 
