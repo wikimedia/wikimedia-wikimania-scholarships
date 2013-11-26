de6340534140c889dff64fb227e488e3f83be009
Antoine Musso <hashar@free.fr>
2013-11-26 13:10:28 +0100

    schol.js: pass jshint
    public/js/schols.js: line 10, col 9, Bad line breaking before '+'.
    public/js/schols.js: line 11, col 5, Bad line breaking before '+'.
    public/js/schols.js: line 12, col 5, Bad line breaking before '+'.
    public/js/schols.js: line 24, col 2, Missing semicolon.
    
    The first three being caused by the string concatenation operator '+'
    being at the beginning of the line.  It should be at the end to
    highlight no semi colon is going to be inserted.
    
    The last error is a missing semicolon at the end of an anonymous
    function.
    
    Change-Id: I79bf8240b598e058448b6c45afd028d595747fa7

--- public/js/schols.js
+++ public/js/schols.js
@@ -1,15 +1,15 @@
 function insertStamp(username) {
 	notes = document.getElementById('notes');
 	now = new Date();
-        year = now.getUTCFullYear();
-        month = now.getUTCMonth() + 1;
-        day = now.getUTCDate();
-        hours = now.getUTCHours();
-        minutes = now.getUTCMinutes();
-        notes.value = month + '/' + day + ' '
-        + hours + ':' + (minutes < 10 ? '0' + minutes : minutes)
-	+ ' ' + "<?= $username['username'] ?>"
-	+ ": \n\n" + notes.value;
+	year = now.getUTCFullYear();
+	month = now.getUTCMonth() + 1;
+	day = now.getUTCDate();
+	hours = now.getUTCHours();
+	minutes = now.getUTCMinutes();
+	notes.value = month + '/' + day + ' ' +
+		hours + ':' + (minutes < 10 ? '0' + minutes : minutes) +
+		' ' + "<?= $username['username'] ?>" +
+		": \n\n" + notes.value;
 }
 
 var menuActive = function() {
@@ -21,7 +21,7 @@ var menuActive = function() {
 		}
 	});
 
-}
+};
 
 $(document).ready(function() {
 	menuActive();
