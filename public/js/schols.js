035616f7c5e5b641aad8e97ec50688c82fb0dca8
Bryan Davis <bd808@wikimedia.org>
2013-11-26 09:43:08 -0700

    Cleanup and fix javascript
    * Add missing jquery ui images to stop 404 requests
    * Fix paths in jquery-ui.css to reference image directory
    * Remove un-minified version of flexigrid plugin
    * Add element ids needed by application dump toggle
    * Remove unused insertStamp() function
    * Remove broken menuActive() function
    
    Bug: 57594
    Change-Id: Id26318eae6533181f73e9958905afe06fb8f51cd

--- public/js/schols.js
+++ public/js/schols.js
@@ -1,30 +1,4 @@
-function insertStamp(username) {
-	notes = document.getElementById('notes');
-	now = new Date();
-	year = now.getUTCFullYear();
-	month = now.getUTCMonth() + 1;
-	day = now.getUTCDate();
-	hours = now.getUTCHours();
-	minutes = now.getUTCMinutes();
-	notes.value = month + '/' + day + ' ' +
-		hours + ':' + (minutes < 10 ? '0' + minutes : minutes) +
-		' ' + "<?= $username['username'] ?>" +
-		": \n\n" + notes.value;
-}
-
-var menuActive = function() {
-	var mainReq = window.location.toString().split(window.location.hostname)[1].split('?')[0];
-
-	$('#review-tabs li a').each(function() {
-		if ( mainReq == $(this).attr('href') ) {
-			$(this).css({'color' : '#0080C0'});
-		}
-	});
-
-};
-
 $(document).ready(function() {
-	menuActive();
 	$('#dump').hide();
 	$('#showfulldump').click(function() {
 		$('#dump').toggle();
