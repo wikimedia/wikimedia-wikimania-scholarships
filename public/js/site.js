9d93e9b10db72f5fe3fadee8cb30034eeacff1f7
Bryan Davis <bd808@wikimedia.org>
2013-12-15 22:35:41 -0700

    Update look and feel
    Part one of a two part look and feel refresh. Add Bootstrap 3.0
    front-end framework. Remove Flexgrid and jQuery UI libraries and support
    files. Add new graphics for 2014 Wikimania.
    
    Change-Id: I647bd55ed1f40381fd85e89fb7e4a0f8cb200a30

--- public/js/site.js
+++ public/js/site.js
@@ -3,7 +3,11 @@
     $('.langlist').each(function() {
       var $list = $(this),
         $select = $(document.createElement('select'))
-          .addClass('langselect')
+          .addClass('form-control')
+          .addClass('input-sm')
+          .change(function() {
+            window.location.href = $(this).find('option:selected').val();
+          })
           .insertBefore($list.hide());
       $('>li a', this).each(function() {
         var $this = $(this);
@@ -11,13 +15,35 @@
           .val(this.href)
           .html($this.html())
           .prop('selected',$this.attr('class')==='selected')
-          .click(function() {
-            window.location.href = $(this).val();
-          })
           .appendTo($select);
       });
       $list.remove();
     });
     $('.langlabel').show();
+
+    /**
+     * Clamped-width.
+     * Usage:
+     *  <div data-clampedwidth=".myParent">This long content will force clamped width</div>
+     *
+     * Author: LV
+     * See: http://stackoverflow.com/a/17243766/8171
+     */
+    $('[data-clampedwidth]').each(function () {
+      var $e = $(this),
+          $parentPanel = $($e.data('clampedwidth')),
+          resizeFn = function () {
+            var sideBarNavWidth = $parentPanel.width() -
+              parseInt($e.css('paddingLeft'), 10) -
+              parseInt($e.css('paddingRight'), 10) -
+              parseInt($e.css('marginLeft'), 10) -
+              parseInt($e.css('marginRight'), 10) -
+              parseInt($e.css('borderLeftWidth'), 10) -
+              parseInt($e.css('borderRightWidth'), 10);
+            $e.css('width', sideBarNavWidth);
+          };
+      resizeFn();
+      $(window).resize(resizeFn);
+    });
   });
 })( jQuery, document, window );
